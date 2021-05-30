390370e6f523578f4d56f694b3871cc8e38e470c
Engin Şenses <73189935+EnginSenses@users.noreply.github.com>
2021-05-31 02:29:20 +0300

    string

--- Flutter Deneme/flutter_application_2/lib/main.dart
+++ Flutter Deneme/flutter_application_2/lib/main.dart
@@ -1,41 +1,25 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Ben());
-}
-
-class Ben extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SafeArea(
-        child: Scaffold(
-          backgroundColor: Colors.blueGrey[600],
-          appBar: AppBar(
-            centerTitle: true,
-            backgroundColor: Colors.blueGrey,
-            title: Text("Engin Şenses"),
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.greenAccent,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey,
+          title: Text(
+            "SA",
           ),
-          body: Container(
-            alignment: Alignment.bottomRight,
-            width: 200,
-            height: 200,
-            decoration: BoxDecoration(
-                shape: BoxShape.rectangle,
-                color: Colors.lime,
-                borderRadius: BorderRadiusDirectional.circular(5)),
-            margin: EdgeInsets.all(15),
-            padding: EdgeInsets.all(25),
-            child: Transform(
-              child: Text('Deneme'),
-              transform: Matrix4.rotationZ(-0.2),
-              alignment: FractionalOffset.center,
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+              "https://i1.sndcdn.com/artworks-000012002480-qainwa-t500x500.jpg",
             ),
           ),
         ),
       ),
-    );
-  }
+    ),
+  );
 }
